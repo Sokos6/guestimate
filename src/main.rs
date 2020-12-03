a00fdc44728d25d2d383aa9806f0e5db52904504
Sokos6 <will.sokolowski@gmail.com>
2020-12-03 16:12:38 -0600

    guessing good

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,10 @@ fn main() {
     println!("Please enter your guess.");
 
     let mut guess = String::new();
+
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
+    println!("You guessed: {}", guess)
 }
